569cd1e58afbb09fe20d179697b82d1f0b5c3335
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-08-09 15:13:13 +0200

    Pretifies shitty styles

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -1,27 +1,30 @@
-import { FaRegPaperPlane } from 'react-icons/fa';
-
+import { FaRegPaperPlane } from "react-icons/fa";
 
 const ContactMe = () => {
-
-  const contactEmail = "sergio.lopez.souto@gmail.com"
+  const contactEmail = "sergio.lopez.souto@gmail.com";
 
   return (
-    <div className="my-10 flex flex-col items-center justify-center w-11/12 md:w-1/2">
-        <h2 className="mb-8 mt-10 text-5xl font-bold tracking-tight text-slate-700 text-center">
+    <div className="mt-[10rem]  my-10 flex flex-col items-center justify-center w-11/12 md:w-1/2">
+      <h2 className="mb-[4rem] text-5xl font-bold tracking-tight text-slate-700 text-center">
         Contact Me
-        </h2>
-        <p className="text-xl font-normal tracking-normal text-slate-600 text-center">
-            I am always open to chat, helping out and open to new projects. Feel free to send me a message!
-        </p>
+      </h2>
+      <p className="text-lg tracking-tight font-normal tracking-normal text-slate-500 text-center">
+        I am always open to chat, helping out and open to new projects. Feel
+        free to send me a message!
+      </p>
 
-      <a href={`mailto:${contactEmail}`} target="_blank" rel="noopener noreferrer">
-        <button className="flex items-center gap-2 bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded mt-8">
+      <a
+        href={`mailto:${contactEmail}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="flex items-center gap-2 bg-gradient-to-b from-amber-600 to bg-orange-600 bg-slate-80 text-white py-2 px-6 py-3 rounded-xl shadow-xl shadow-amber-600/50 hover:-translate-y-1 mt-8 transition-all">
           <FaRegPaperPlane />
-          Send me an Email
+          Email me
         </button>
       </a>
-    </div>  
-  )
-}
+    </div>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,22 +1,20 @@
-import { projects } from '@/projects';
-import ProjectCard from "./ProjectCard"
-
+import { projects } from "@/projects";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
-
   return (
-    <div className="my-10 flex flex-col items-center justify-center md:w-1/2">
-        <h2 className="mb-8 mt-10 text-5xl font-bold tracking-tight text-slate-700 text-center">Projects</h2>
-        
-        <div className="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-          {
-          projects.map((project, index) => (
-              <ProjectCard key={index} {...project} />
-          ))
-          }
-        </div>
+    <div className="mt-[15rem] my-10 flex flex-col items-center justify-center md:w-1/2">
+      <h2 className="mb-[5rem] mt-10 text-5xl font-bold tracking-tight text-slate-700 text-center">
+        Projects
+      </h2>
+
+      <div className="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
