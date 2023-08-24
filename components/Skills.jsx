d04a13e5577555de3e90b5e2f7c9c0996a71c615
sergiolopez <sergio.lopez.souto@gmail.com>
2023-08-24 19:52:08 +0200

    improving skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,45 +1,46 @@
+'use client'
+import React from 'react';
 import { SiPython, SiJavascript, SiTypescript, SiReact, SiNextdotjs, SiTailwindcss, SiNodedotjs, SiExpress, SiMysql, SiPostgresql, SiMongodb, SiGit } from 'react-icons/si';
-// import './Skills.css'
-
+import './Skills.css'; // Asegúrate de tener este archivo CSS
 
 const Skills = () => {
   return (
-    <div className="my-10 overflow-hidden">
-      <div className="flex justify-center flex-wrap gap-4 mt-14 animate-slide mx-0">
+    <div className="my-20 overflow-hidden">
+      <div className="skills-list animate-slide-infinite">
         <span className="m-3 inline-block text-gray-500 hover:text-black">
           <SiPython size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-yellow-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-yellow-400">
           <SiJavascript size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-blue-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-blue-400">
           <SiTypescript size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-blue-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-blue-400">
           <SiReact size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-black">
+        <span className="m-3 inline-block text-gray-500 hover:text-black">
           <SiNextdotjs size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-blue-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-blue-400">
           <SiTailwindcss size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-green-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-green-400">
           <SiNodedotjs size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-black">
+        <span className="m-3 inline-block text-gray-500 hover:text-black">
           <SiExpress size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-blue-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-blue-400">
           <SiMysql size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-blue-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-blue-400">
           <SiPostgresql size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-green-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-green-400">
           <SiMongodb size={50} />
         </span>
-        <span className="my-3 mx-5 inline-block text-gray-500 hover:text-red-400">
+        <span className="m-3 inline-block text-gray-500 hover:text-red-400">
           <SiGit size={50} />
         </span>
       </div>
@@ -48,3 +49,5 @@ const Skills = () => {
 }
 
 export default Skills;
+
+
