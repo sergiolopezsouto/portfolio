6376fa53b566df355d382641af4d5b2c6fa45120
sergiolopez <sergio.lopez.souto@gmail.com>
2023-08-24 15:01:18 +0200

    improving contact

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -6,9 +6,9 @@ const ContactMe = () => {
   const contactEmail = "sergio.lopez.souto@gmail.com"
 
   return (
-    <div className="my-10 flex flex-col items-center justify-center w-11/12 md:w-1/2">
+    <div className="my-20 flex flex-col items-center justify-center w-11/12 md:w-1/2">
         <h2 className="mb-8 mt-10 text-5xl font-bold tracking-tight text-slate-700 text-center">
-        Contact Me
+            Contact
         </h2>
         <p className="text-xl font-normal tracking-normal text-slate-600 text-center">
             I am always open to chat, helping out and open to new projects. Feel free to send me a message!
